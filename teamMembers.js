a989ab8035057914c7a1c5c147747cb384ef7b98
tlnguyen2009 <tienlucnguyen2009@gmail.com>
2025-10-07 15:28:34 -0400

    Add files via upload

---
+++ teamMembers.js
@@ -0,0 +1,27 @@
+export const teamMembers = [
+    {
+      name: "Aaron Waller",
+      bio: "Passionate about game mechanics and clean code",
+      avatar: "/AW_avatar.png",
+    },
+    {
+      name: "Benedict Antwi",
+      bio: "Passionate about game mechanics and clean code",
+      avatar: "/BA_avatar.png",
+    },
+    {
+      name: "Tien Nguyen",
+      bio: "Passionate about game mechanics and clean code",
+      avatar: "/TN_avatar.png",
+    },
+    {
+      name: "Aditya Patel",
+      bio: "Passionate about game mechanics and clean code",
+      avatar: "/AP_avatar.png",
+    },
+    {
+      name: "Aishwarya Silam",
+      bio: "Passionate about game mechanics and clean code",
+      avatar: "/AS_avatar.png",
+    },
+]
